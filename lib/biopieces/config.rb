f64a330c68c3a79dbe0ce0cde222a29dc18ca8d6
Martin Asser Hansen <mail@maasha.dk>
2014-10-09 23:52:02 +0200

    adjusted SORT_BLOCK_SIZE

--- lib/biopieces/config.rb
+++ lib/biopieces/config.rb
@@ -31,6 +31,6 @@ module BioPieces
     STATUS_SAVE_INTERVAL = 1           # save status every n second.
     DEBUG                = false
     SCORES_MAX           = 100_000     # maximum score string length in plot_scores.
-    SORT_BLOCK_SIZE      = 500_000_000 # max bytes to hold in memory when sorting.
+    SORT_BLOCK_SIZE      = 250_000_000 # max bytes to hold in memory when sorting.
   end
 end
