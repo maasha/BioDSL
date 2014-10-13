a4732839b41e940ad56f869cc240aaa44cbec2a7
Martin Asser Hansen <mail@maasha.dk>
2014-10-13 15:37:43 +0200

    added usearch_global command

--- test/biopieces/commands/test_usearch_global.rb
+++ test/biopieces/commands/test_usearch_global.rb
@@ -1,3 +1,6 @@
+#!/usr/bin/env ruby
+$:.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
+
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
 #                                                                                #
 # Copyright (C) 2007-2014 Martin Asser Hansen (mail@maasha.dk).                  #
@@ -24,26 +27,40 @@
 #                                                                                #
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
 
-module BioPieces
-  module Commands
-    require 'biopieces/commands/add_key'
-    require 'biopieces/commands/assemble_pairs'
-    require 'biopieces/commands/clip_primer'
-    require 'biopieces/commands/cluster_otus'
-    require 'biopieces/commands/dereplicate_seq'
-    require 'biopieces/commands/dump'
-    require 'biopieces/commands/grab'
-    require 'biopieces/commands/mean_scores'
-    require 'biopieces/commands/plot_histogram'
-    require 'biopieces/commands/plot_scores'
-    require 'biopieces/commands/read_fasta'
-    require 'biopieces/commands/read_fastq'
-    require 'biopieces/commands/sort'
-    require 'biopieces/commands/trim_primer'
-    require 'biopieces/commands/trim_seq'
-    require 'biopieces/commands/uchime_ref'
-    require 'biopieces/commands/usearch_global'
-    require 'biopieces/commands/write_fasta'
-    require 'biopieces/commands/write_fastq'
+require 'test/helper'
+
+class TestUchimeRef < Test::Unit::TestCase 
+  def setup
+    @db = File.join(File.dirname(__FILE__), '..', '..', '..', 'data', 'chimera_db.fna')
+  end
+
+  test "BioPieces::Pipeline#uchime_ref with disallowed option raises" do
+    p = BioPieces::Pipeline.new
+    assert_raise(BioPieces::OptionError) { p.uchime_ref(foo: "bar") }
+  end
+
+  test "BioPieces::Pipeline#uchime_ref with allowed option don't raise" do
+    p = BioPieces::Pipeline.new
+    assert_nothing_raised { p.uchime_ref(database: @db) }
+  end
+
+  test "BioPieces::Pipeline#uchime_ref outputs correctly" do
+    input, output   = BioPieces::Stream.pipe
+    input2, output2 = BioPieces::Stream.pipe
+
+    output.write({one: 1, two: 2, three: 3})
+    output.write({SEQ_COUNT: 5, SEQ: "atcgaAcgatcgatcgatcgatcgatcgtacgacgtagct"})
+    output.write({SEQ_COUNT: 4, SEQ: "atcgatcgatcgatcgatcgatcgatcgtacgacgtagct"})
+    output.close
+
+    p = BioPieces::Pipeline.new
+    p.uchime_ref(database: @db).run(input: input, output: output2)
+    result   = input2.map { |h| h.to_s }.reduce(:<<)
+    expected = ""
+    expected << %Q{{:one=>1, :two=>2, :three=>3}}
+    expected << %Q{{:SEQ_NAME=>\"1\", :SEQ=>\"atcgaAcgatcgatcgatcgatcgatcgtacgacgtagct\", :SEQ_LEN=>40}}
+    expected << %Q{{:SEQ_NAME=>\"2\", :SEQ=>\"atcgatcgatcgatcgatcgatcgatcgtacgacgtagct\", :SEQ_LEN=>40}}
+
+    assert_equal(expected, result)
   end
 end
