5e606946784d16f1babe1dc069bb3c80f6c61051
Martin Asser Hansen <mail@maasha.dk>
2014-10-28 16:32:50 +0100

    created merge_table command

--- lib/biopieces/commands.rb
+++ lib/biopieces/commands.rb
@@ -36,6 +36,7 @@ module BioPieces
     require 'biopieces/commands/dump'
     require 'biopieces/commands/grab'
     require 'biopieces/commands/mean_scores'
+    require 'biopieces/commands/merge_table'
     require 'biopieces/commands/merge_values'
     require 'biopieces/commands/plot_histogram'
     require 'biopieces/commands/plot_matches'
