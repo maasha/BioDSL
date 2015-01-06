30f331657a0bcca4e1d26e9b1bf63396ef6e4856
Martin Asser Hansen <mail@maasha.dk>
2015-01-06 21:19:34 +0100

    added store and retrieve methods to cary

--- test/biopieces/test_cary.rb
+++ test/biopieces/test_cary.rb
@@ -44,6 +44,10 @@ class TestCAry < Test::Unit::TestCase
     assert_equal("0" * 40, BioPieces::CAry.new(5, 1).to_s)
   end
 
+  test "BioPieces::CAry.new with ary returns correctly" do
+    assert_equal([1,2].pack("I*").unpack("B*").first, BioPieces::CAry.new(2, 4, [1,2].pack("I*")).to_s)
+  end
+
   test "BioPieces::CAry.fill! returns correctly" do
     cary = BioPieces::CAry.new(5, 1)
     cary.fill!
@@ -149,6 +153,20 @@ class TestCAry < Test::Unit::TestCase
 
     assert_equal("1" * 40, cary1.to_s)
     assert_equal("1" * 40, cary2.to_s)
+  end
+
+  test "BioPieces::CAry #store and #retrieve returns correctly" do
+    file = Tempfile.new('cary')
+    cary = BioPieces::CAry.new(5, 1).fill
+
+    begin
+      BioPieces::CAry.store(file, cary)
+      cary2 = BioPieces::CAry.retrieve(file)
+      assert_equal(cary.to_s, cary2.to_s)
+    ensure
+      file.close
+      file.unlink
+    end
   end
 end
 
