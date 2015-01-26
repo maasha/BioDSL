ec23a3f62c51bfa1e771007b50f79465e9a6c513
Martin A. Hansen <mail@maasha.dk>
2015-01-26 16:31:02 +0100

    added a few tests to assemble_seq_spades

---
+++ test/biopieces/commands/test_assemble_seq_spades.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+$:.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                                #
+# Copyright (C) 2007-2014 Martin Asser Hansen (mail@maasha.dk).                  #
+#                                                                                #
+# This program is free software; you can redistribute it and/or                  #
+# modify it under the terms of the GNU General Public License                    #
+# as published by the Free Software Foundation; either version 2                 #
+# of the License, or (at your option) any later version.                         #
+#                                                                                #
+# This program is distributed in the hope that it will be useful,                #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
+# GNU General Public License for more details.                                   #
+#                                                                                #
+# You should have received a copy of the GNU General Public License              #
+# along with this program; if not, write to the Free Software                    #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. #
+#                                                                                #
+# http://www.gnu.org/copyleft/gpl.html                                           #
+#                                                                                #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                                #
+# This software is part of Biopieces (www.biopieces.org).                        #
+#                                                                                #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+
+require 'test/helper'
+
+class TestAssembleSeqSpades < Test::Unit::TestCase 
+  def setup
+    @p = BioPieces::Pipeline.new
+  end
+
+  test "BioPieces::Pipeline::AssembleSeqSpades with invalid options raises" do
+    assert_raise(BioPieces::OptionError) { @p.assemble_seq_spades(foo: "bar") }
+  end
+
+  test "BioPieces::Pipeline::AssembleSeqSpades with valid options don't raise" do
+    assert_nothing_raised { @p.assemble_seq_spades(cpus: 1) }
+  end
+
+  #TODO Fix missing testing here!
+end
