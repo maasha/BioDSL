746a333ef7d69fc400954d219dc6548a3cb92827
Martin A. Hansen <mail@maasha.dk>
2015-03-30 15:25:27 +0200

    testing begun on option_helpers

--- lib/biopieces/helpers/options_helper.rb
+++ lib/biopieces/helpers/options_helper.rb
@@ -1,48 +1,68 @@
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                  #
-#                                                                                #
-# This program is free software; you can redistribute it and/or                  #
-# modify it under the terms of the GNU General Public License                    #
-# as published by the Free Software Foundation; either version 2                 #
-# of the License, or (at your option) any later version.                         #
-#                                                                                #
-# This program is distributed in the hope that it will be useful,                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-# GNU General Public License for more details.                                   #
-#                                                                                #
-# You should have received a copy of the GNU General Public License              #
-# along with this program; if not, write to the Free Software                    #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. #
-#                                                                                #
-# http://www.gnu.org/copyleft/gpl.html                                           #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# This software is part of Biopieces (www.biopieces.org).                        #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                #
+#                                                                              #
+# This program is free software; you can redistribute it and/or                #
+# modify it under the terms of the GNU General Public License                  #
+# as published by the Free Software Foundation; either version 2               #
+# of the License, or (at your option) any later version.                       #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,    #
+# USA.                                                                         #
+#                                                                              #
+# http://www.gnu.org/copyleft/gpl.html                                         #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# This software is part of Biopieces (www.biopieces.org).                      #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
 
 module BioPieces
+  # Module containing methods to check options in various ways.
   module OptionsHelper
-    class BioPieces::OptionError < StandardError; end;
+    BioPieces::OptionsError = Class.new(StandardError)
 
-    # Method that raises if options include any option not in the allowed list.
+    # Method that raises if options include any option not in the allowed Array.
+    #
+    # options - The Hash with options to be checked.
+    # allowed - One or more allowed options.
+    #
+    # Examples
+    #
+    #   options_allowed({foo: "bar"}, :foo)
+    #   # => nil
+    #
+    #   options_allowed({foo: "bar"}, :foobar)
+    #   # => Raises
+    #
+    # Returns nothing.
+    # Raises BioPieces::OptionError on any options not in the allowed Array.
     def options_allowed(options, *allowed)
       options.each_key do |option|
         unless allowed.include? option
-          raise BioPieces::OptionError, "Disallowed option: #{option}. Allowed options: #{allowed.join(", ")}"
+          fail BioPieces::OptionError, "Disallowed option: #{option}. Allowed" \
+            "options: #{allowed.join(', ')}"
         end
       end
+
+      nil
     end
 
-    # Method that raises of options include any option value not in the allowed hash.
+    # Method that raises of options include any option value not in the allowed
+    # hash.
     def options_allowed_values(options, allowed)
       allowed.each do |key, values|
         if options[key]
           unless values.include? options[key]
-            raise BioPieces::OptionError, "Disallowed option value: #{options[key]}. Allowed options: #{values.join(", ")}"
+            fail BioPieces::OptionError, "Disallowed option value: #{options[key]}. Allowed options: #{values.join(', ')}"
           end
         end
       end
@@ -52,7 +72,7 @@ module BioPieces
     def options_required(options, *required)
       required.each do |option|
         unless options[option]
-          raise BioPieces::OptionError, "Required option missing: #{option}. Required options: #{required.join(", ")}"
+          fail BioPieces::OptionError, "Required option missing: #{option}. Required options: #{required.join(', ')}"
         end
       end
     end
@@ -66,9 +86,9 @@ module BioPieces
       end
 
       if lookup.size > 1
-        raise BioPieces::OptionError, "Multiple required uniques options used: #{unique.join(", ")}"
+        fail BioPieces::OptionError, "Multiple required uniques options used: #{unique.join(', ')}"
       elsif lookup.size == 0
-        raise BioPieces::OptionError, "Required unique option missing: #{unique.join(", ")}"
+        fail BioPieces::OptionError, "Required unique option missing: #{unique.join(', ')}"
       end
     end
 
@@ -80,9 +100,9 @@ module BioPieces
         lookup << option if options[option]
       end
 
-      if lookup.size == 0
-        raise BioPieces::OptionError, "Required single option missing: #{single.join(", ")}"
-      end
+      return unless lookup.size == 0
+
+      fail BioPieces::OptionError, "Required single option missing: #{single.join(', ')}"
     end
 
     # Method that raises if options include multiple options in the unique list.
@@ -93,17 +113,16 @@ module BioPieces
         lookup << option if options[option]
       end
 
-      if lookup.size > 1
-        raise BioPieces::OptionError, "Multiple uniques options used: #{unique.join(", ")}"
-      end
+      return unless lookup.size > 1
+      fail BioPieces::OptionError, "Multiple uniques options used: #{unique.join(', ')}"
     end
 
     # Method that raises if options include lists with duplicate elements.
     # Usage options_unique_list(options, :keys, :skip)
     def options_list_unique(options, *lists)
       lists.each do |list|
-        if options[list] and options[list].uniq.size != options[list].size
-          raise BioPieces::OptionError, "Duplicate elements found in list #{list}: #{options[list]}"
+        if options[list] && options[list].uniq.size != options[list].size
+          fail BioPieces::OptionError, "Duplicate elements found in list #{list}: #{options[list]}"
         end
       end
     end
@@ -111,7 +130,7 @@ module BioPieces
     # Method to expand all options in the glob list into lists of paths.
     def options_glob(options, *globs)
       globs.each do |option|
-        if options[option] and not options[option].is_a? Array
+        if options[option] && !options[option].is_a?(Array)
           expanded_paths = []
           options[option].split(/, */).each do |glob_expression|
             if glob_expression.include? '*'
@@ -130,8 +149,8 @@ module BioPieces
     # Example: options_tie gzip: :options, bzip2: :options
     def options_tie(options, ties)
       ties.each do |option, tie|
-        if options[option] and not options[tie]
-          raise BioPieces::OptionError, "Tie option: #{tie} not in @options: #{options.keys.join(", ")}"
+        if options[option] && !options[tie]
+          fail BioPieces::OptionError, "Tie option: #{tie} not in @options: #{options.keys.join(', ')}"
         end
       end
     end
@@ -140,8 +159,8 @@ module BioPieces
     # Example: select: :evaluate, reject: :evaluate
     def options_conflict(options, conflicts)
       conflicts.each do |option, conflict|
-        if options[option] and options[conflict]
-          raise BioPieces::OptionError, "Conflicting options: #{option}, #{conflict}"
+        if options[option] && options[conflict]
+          fail BioPieces::OptionError, "Conflicting options: #{option}, #{conflict}"
         end
       end
     end
@@ -155,7 +174,7 @@ module BioPieces
 
           files.each do |file|
             unless File.file? File.expand_path(file)
-              raise BioPieces::OptionError, "For option #{arg} - no such file: #{file}"
+              fail BioPieces::OptionError, "For option #{arg} - no such file: #{file}"
             end
           end
         end
@@ -171,7 +190,7 @@ module BioPieces
 
           dirs.each do |dir|
             unless File.directory? File.expand_path(dir)
-              raise BioPieces::OptionError, "For option #{arg} - no such directory: #{dir}"
+              fail BioPieces::OptionError, "For option #{arg} - no such directory: #{dir}"
             end
           end
         end
@@ -186,8 +205,8 @@ module BioPieces
           files = (options[arg].is_a? Array) ? options[arg] : [options[arg]]
 
           files.each do |file|
-            if File.file? file and not options[:force]
-              raise BioPieces::OptionError, "File exists: #{file} - use 'force: true' to override"
+            if File.file?(file) && !options[:force]
+              fail BioPieces::OptionError, "File exists: #{file} - use 'force: true' to override"
             end
           end
         end
@@ -201,28 +220,29 @@ module BioPieces
         expression.gsub!(/:#{key}/, value.to_s)
       end
 
-      unless expression =~ /:\w/
-        unless eval expression
-          raise BioPieces::OptionError, "Assertion failed: #{expression}"
-        end
-      end
+      return if expression[0] == ':'
+      return if eval expression
+
+      fail BioPieces::OptionError, "Assertion failed: #{expression}"
     end
 
     def options_load_rc(options, command)
-      rc_file = File.join(ENV['HOME'], ".biopiecesrc")
+      rc_file = File.join(ENV['HOME'], '.biopiecesrc')
 
-      if File.exist? rc_file
-        File.open(rc_file) do |ios|
-          ios.each do |line|
-            line.chomp!
+      return unless File.exist? rc_file
 
-            next if line.empty?
-            next if line[0] == '#'
+      File.open(rc_file) do |ios|
+        ios.each do |line|
+          line.chomp!
 
-            fields = line.split(/\s+/)
+          next if line.empty?
+          next if line[0] == '#'
 
-            if fields.first.to_sym == command
-              options[fields[1].to_sym] = fields[2] unless options[fields[1].to_sym]
+          fields = line.split(/\s+/)
+
+          if fields.first.to_sym == command
+            unless options[fields[1].to_sym]
+              options[fields[1].to_sym] = fields[2]
             end
           end
         end
@@ -230,4 +250,3 @@ module BioPieces
     end
   end
 end
-
---
+++ test/biopieces/helpers/test_options_helper.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+$:.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                                #
+# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                  #
+#                                                                                #
+# This program is free software; you can redistribute it and/or                  #
+# modify it under the terms of the GNU General Public License                    #
+# as published by the Free Software Foundation; either version 2                 #
+# of the License, or (at your option) any later version.                         #
+#                                                                                #
+# This program is distributed in the hope that it will be useful,                #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
+# GNU General Public License for more details.                                   #
+#                                                                                #
+# You should have received a copy of the GNU General Public License              #
+# along with this program; if not, write to the Free Software                    #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. #
+#                                                                                #
+# http://www.gnu.org/copyleft/gpl.html                                           #
+#                                                                                #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                                #
+# This software is part of Biopieces (www.biopieces.org).                        #
+#                                                                                #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+
+require 'test/helper'
+
+class TestOptionsHelper < Test::Unit::TestCase 
+  include BioPieces::OptionsHelper
+
+  test "#options_allowed with disallowed option raises" do
+    options = {bar: "foo"}
+    assert_raise(BioPieces::OptionError) { options_allowed(options, :foo) }
+  end
+
+  test "#options_allowed with allowed option don't raise" do
+    options = {foo: "bar"}
+    assert_nothing_raised { options_allowed(options, :foo) }
+  end
+
+  test "#options_allowed with no options don't raise" do
+    options = {}
+    assert_nothing_raised { options_allowed(options, :foo) }
+  end
+end
