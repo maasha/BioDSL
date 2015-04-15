ea4551b71f2b31861981c21329a4c929a694665c
Martin A. Hansen <mail@maasha.dk>
2015-04-15 16:43:46 +0200

    rewrite of write_fasta command complete

--- lib/biopieces/commands/write_fasta.rb
+++ lib/biopieces/commands/write_fasta.rb
@@ -1,151 +1,231 @@
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                  #
-#                                                                                #
-# This program is free software; you can redistribute it and/or                  #
-# modify it under the terms of the GNU General Public License                    #
-# as published by the Free Software Foundation; either version 2                 #
-# of the License, or (at your option) any later version.                         #
-#                                                                                #
-# This program is distributed in the hope that it will be useful,                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-# GNU General Public License for more details.                                   #
-#                                                                                #
-# You should have received a copy of the GNU General Public License              #
-# along with this program; if not, write to the Free Software                    #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. #
-#                                                                                #
-# http://www.gnu.org/copyleft/gpl.html                                           #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# This software is part of the Biopieces framework (www.biopieces.org).          #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                #
+#                                                                              #
+# This program is free software; you can redistribute it and/or                #
+# modify it under the terms of the GNU General Public License                  #
+# as published by the Free Software Foundation; either version 2               #
+# of the License, or (at your option) any later version.                       #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,    #
+# USA.                                                                         #
+#                                                                              #
+# http://www.gnu.org/copyleft/gpl.html                                         #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# This software is part of the Biopieces framework (www.biopieces.org).        #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
 
 module BioPieces
-  module Commands
-    # == Write sequences from stream in FASTA format.
-    # 
-    # Description
-    # 
-    # +write_fasta+ writes sequence from the data stream in FASTA format.
-    # However, a FASTA entry will only be written if a SEQ key and a SEQ_NAME key
-    # is present. An example FASTA entry:
+  # == Write sequences from stream in FASTA format.
+  #
+  # Description
+  #
+  # +write_fasta+ writes sequence from the data stream in FASTA format.
+  # However, a FASTA entry will only be written if a SEQ key and a SEQ_NAME key
+  # is present. An example FASTA entry:
+  #
+  #     >test1
+  #     TATGACGCGCATCGACAGCAGCACGAGCATGCATCGACTG
+  #     TGCACTGACTACGAGCATCACTATATCATCATCATAATCT
+  #     TACGACATCTAGGGACTAC
+  #
+  # For more about the FASTA format:
+  #
+  # http://en.wikipedia.org/wiki/FASTA_format
+  #
+  # == Usage
+  #    write_fasta([wrap: <uin>[, output: <file>[, force: <bool>
+  #                [, gzip: <bool> | bzip2: <bool>]]]])
+  #
+  # === Options
+  # * output <file> - Output file.
+  # * force <bool>  - Force overwrite existing output file.
+  # * wrap <uint>   - Wrap sequence into lines of wrap length.
+  # * gzip <bool>   - Write gzipped output file.
+  # * bzip2 <bool>  - Write bzipped output file.
+  #
+  # == Examples
+  #
+  # To write FASTA entries to STDOUT.
+  #
+  #    write_fasta
+  #
+  # To write FASTA entries wrapped in lines of length of 80 to STDOUT.
+  #
+  #    write_fasta(wrap: 80)
+  #
+  # To write FASTA entries to a file 'test.fna'.
+  #
+  #    write_fasta(output: "test.fna")
+  #
+  # To overwrite output file if this exists use the force option:
+  #
+  #    write_fasta(output: "test.fna", force: true)
+  #
+  # To write gzipped FASTA entries to file 'test.fna.gz'.
+  #
+  #    write_fasta(output: "test.fna.gz", gzip: true)
+  #
+  # To write bzipped FASTA entries to file 'test.fna.bz2'.
+  #
+  #    write_fasta(output: "test.fna.bz2", bzip2: true)
+  class WriteFasta
+    require 'biopieces/helpers/options_helper'
+    extend OptionsHelper
+    include OptionsHelper
+
+    # Check the options and return a lambda for the command.
     #
-    #     >test1
-    #     TATGACGCGCATCGACAGCAGCACGAGCATGCATCGACTG
-    #     TGCACTGACTACGAGCATCACTATATCATCATCATAATCT
-    #     TACGACATCTAGGGACTAC
-    # 
-    # For more about the FASTA format:
-    # 
-    # http://en.wikipedia.org/wiki/FASTA_format
-    # 
-    # == Usage
-    #    write_fasta([wrap: <uin>[, output: <file>[, force: <bool>
-    #                [, gzip: <bool> | bzip2: <bool>]]]])
+    # @param [Hash] options Options hash.
+    # @option options [Bool] :force Flag allowing overwriting files.
+    # @option options [String] :output Output file path.
+    # @option options [Integer] :wrap Wrap sequences at this length (default no
+    #   wrap)
+    # @option options [Bool] :gzip Output will be gzip'ed.
+    # @option options [Bool] :bzip2 Output will be bzip2'ed.
     #
-    # === Options
-    # * output <file> - Output file.
-    # * force <bool>  - Force overwrite existing output file.
-    # * wrap <uint>   - Wrap sequence into lines of wrap length.
-    # * gzip <bool>   - Write gzipped output file.
-    # * bzip2 <bool>  - Write bzipped output file.
-    # 
-    # == Examples
-    # 
-    # To write FASTA entries to STDOUT.
-    # 
-    #    write_fasta
-    #
-    # To write FASTA entries wrapped in lines of length of 80 to STDOUT.
-    # 
-    #    write_fasta(wrap: 80)
-    # 
-    # To write FASTA entries to a file 'test.fna'.
-    # 
-    #    write_fasta(output: "test.fna")
-    # 
-    # To overwrite output file if this exists use the force option:
-    #
-    #    write_fasta(output: "test.fna", force: true)
-    #
-    # To write gzipped FASTA entries to file 'test.fna.gz'.
-    # 
-    #    write_fasta(output: "test.fna.gz", gzip: true)
-    #
-    # To write bzipped FASTA entries to file 'test.fna.bz2'.
-    # 
-    #    write_fasta(output: "test.fna.bz2", bzip2: true)
-    def write_fasta(options = {})
-      options_orig = options.dup
+    # @return [Proc] Returns the command lambda.
+    def self.lmb(options)
       options_load_rc(options, __method__)
       options_allowed(options, :force, :output, :wrap, :gzip, :bzip2)
       options_unique(options, :gzip, :bzip2)
       options_tie(options, gzip: :output, bzip2: :output)
-      options_files_exists_force(options, :output)
+      options_files_exist_force(options, :output)
 
-      lmb = lambda do |input, output, status|
-        status_track(status) do
-          status[:sequences_out] = 0
-          status[:residues_out]  = 0
+      new(options).lmb
+    end
 
-          options[:output] ||= $stdout
+    # Constructor for the WriteFasta class.
+    #
+    # @param [Hash] options Options hash.
+    # @option options [Bool] :force Flag allowing overwriting files.
+    # @option options [String] :output Output file path.
+    # @option options [Integer] :wrap Wrap sequences at this length (default no
+    #   wrap)
+    # @option options [Bool] :gzip Output will be gzip'ed.
+    # @option options [Bool] :bzip2 Output will be bzip2'ed.
+    #
+    # @return [WriteFasta] Returns an instance of the class.
+    def initialize(options)
+      @options       = options
+      @records_in    = 0
+      @records_out   = 0
+      @sequences_in  = 0
+      @sequences_out = 0
+      @residues_in   = 0
+      @residues_out  = 0
 
-          if options[:output] === $stdout
-            input.each do |record|
-              status[:records_in] += 1
+      @options[:output] ||= $stdout
+    end
 
-              if record[:SEQ_NAME] and record[:SEQ]
-                entry = BioPieces::Seq.new_bp(record)
+    # Return a lambda for the write_fasta command.
+    #
+    # @return [Proc] Returns the write_fasta command lambda.
+    def lmb
+      lambda do |input, output, status|
+        if @options[:output] == $stdout
+          write_stdout(input, output)
+        else
+          write_file(input, output)
+        end
 
-                $stdout.puts entry.to_fasta(options[:wrap])
-                status[:sequences_out] += 1
-                status[:residues_out]  += entry.length
-              end
+        assign_status(status)
+      end
+    end
 
-              if output
-                output << record
-                status[:records_out] += 1
-              end
-            end
-          else
-            if options[:gzip]
-              compress = :gzip
-            elsif options[:bzip2]
-              compress = :bzip2
-            else
-              compress = nil
-            end
+    private
 
-            Fasta.open(options[:output], 'w', compress: compress) do |ios|
-              input.each do |record|
-                status[:records_in] += 1
+    # Write all sequence entries to stdout.
+    #
+    # @param input  [Enumerator::Yielder] The input stream.
+    # @param output [Enumerator::Yielder] The output stream.
+    def write_stdout(input, output)
+      wrap = @options[:wrap]
 
-                if record[:SEQ_NAME] and record[:SEQ]
-                  entry = BioPieces::Seq.new_bp(record)
+      input.each do |record|
+        @records_in += 1
 
-                  ios.puts entry.to_fasta(options[:wrap])
-                  status[:sequences_out] += 1
-                  status[:residues_out]  += entry.length
-                end
+        if (entry = record2entry(record))
+          $stdout.puts entry.to_fasta(wrap)
+          @sequences_out += 1
+          @residues_out  += entry.length
+        end
 
-                if output
-                  output << record
-                  status[:records_out] += 1
-                end
-              end
-            end
+        write_output(output, record)
+      end
+    end
+
+    # Write all sequence entries to a specified file.
+    #
+    # @param input  [Enumerator::Yielder] The input stream.
+    # @param output [Enumerator::Yielder] The output stream.
+    def write_file(input, output)
+      Fasta.open(@options[:output], 'w', compress: compress) do |ios|
+        input.each do |record|
+          @records_in += 1
+
+          if (entry = record2entry(record))
+            ios.puts entry.to_fasta(@options[:wrap])
+            @sequences_out += 1
+            @residues_out  += entry.length
           end
+
+          write_output(output, record)
         end
       end
+    end
 
-      @commands << BioPieces::Pipeline::Command.new(__method__, options, options_orig, lmb)
+    # Write a given record to the output stream if this exist.
+    #
+    # @param output [Enumerator::Yielder, nil] Output stream.
+    # @param record [Hash] Biopices record to write.
+    def write_output(output, record)
+      return unless output
 
-      self
+      output << record
+      @records_out += 1
+    end
+
+    # Creates a Seq object from a given record if SEQ_NAME and SEQ is present.
+    #
+    # @param record [Hash] Biopices record to convert.
+    #
+    # @return [BioPieces::Seq] Sequence entry.
+    def record2entry(record)
+      return unless record.key? :SEQ_NAME
+      return unless record.key? :SEQ
+
+      BioPieces::Seq.new_bp(record)
+    end
+
+    # Determine what compression should be used for output.
+    #
+    # @return [Symbol, nil] Compression flag or nil if no compression.
+    def compress
+      return :gzip  if @options[:gzip]
+      return :bzip2 if @options[:bzip2]
+    end
+
+    # Assign all stats to the status hash.
+    #
+    # @param status [Hash] Status hash.
+    def assign_status(status)
+      status[:records_in]    = @records_in
+      status[:records_out]   = @records_out
+      status[:sequences_in]  = @sequences_in
+      status[:sequences_out] = @sequences_out
+      status[:residues_in]   = @residues_in
+      status[:residues_out]  = @residues_out
     end
   end
 end
-
--- test/biopieces/commands/test_write_fasta.rb
+++ test/biopieces/commands/test_write_fasta.rb
@@ -1,137 +1,155 @@
 #!/usr/bin/env ruby
-$:.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                  #
-#                                                                                #
-# This program is free software; you can redistribute it and/or                  #
-# modify it under the terms of the GNU General Public License                    #
-# as published by the Free Software Foundation; either version 2                 #
-# of the License, or (at your option) any later version.                         #
-#                                                                                #
-# This program is distributed in the hope that it will be useful,                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-# GNU General Public License for more details.                                   #
-#                                                                                #
-# You should have received a copy of the GNU General Public License              #
-# along with this program; if not, write to the Free Software                    #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. #
-#                                                                                #
-# http://www.gnu.org/copyleft/gpl.html                                           #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# This software is part of Biopieces (www.biopieces.org).                        #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                #
+#                                                                              #
+# This program is free software; you can redistribute it and/or                #
+# modify it under the terms of the GNU General Public License                  #
+# as published by the Free Software Foundation; either version 2               #
+# of the License, or (at your option) any later version.                       #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,    #
+# USA.                                                                         #
+#                                                                              #
+# http://www.gnu.org/copyleft/gpl.html                                         #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# This software is part of Biopieces (www.biopieces.org).                      #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
 
 require 'test/helper'
 
-class TestWriteFasta < Test::Unit::TestCase 
+# Test class for WriteFasta.
+class TestWriteFasta < Test::Unit::TestCase
   def setup
-    @zcat = BioPieces::Filesys::which('gzcat') || BioPieces::Filesys::which('zcat')
+    @zcat = BioPieces::Filesys.which('gzcat') ||
+            BioPieces::Filesys.which('zcat')
 
-    @tmpdir = Dir.mktmpdir("BioPieces")
+    init_test_files
+    init_data_streams
+
+    @expected = <<-EXP.gsub(/^\s+\|/, '')
+      |>test1
+      |atcg
+      |>test2
+      |gtac
+    EXP
+
+    @p = BioPieces::Pipeline.new
+    @e = BioPieces::OptionError
+  end
+
+  def init_test_files
+    @tmpdir = Dir.mktmpdir('BioPieces')
     @file   = File.join(@tmpdir, 'test.fna')
     @file2  = File.join(@tmpdir, 'test.fna')
+  end
 
+  def init_data_streams
     @input, @output   = BioPieces::Stream.pipe
     @input2, @output2 = BioPieces::Stream.pipe
 
-    hash1 = {SEQ_NAME: "test1", SEQ: "atcg", SEQ_LEN: 4}
-    hash2 = {SEQ_NAME: "test2", SEQ: "gtac", SEQ_LEN: 4}
-
-    @output.write hash1
-    @output.write hash2
+    @output.write(SEQ_NAME: 'test1', SEQ: 'atcg', SEQ_LEN: 4)
+    @output.write(SEQ_NAME: 'test2', SEQ: 'gtac', SEQ_LEN: 4)
     @output.close
-
-    @p = BioPieces::Pipeline.new
   end
 
   def teardown
     FileUtils.rm_r @tmpdir
   end
 
-  test "BioPieces::Pipeline::WriteFasta with invalid options raises" do
-    assert_raise(BioPieces::OptionError) { @p.write_fasta(foo: "bar") }
+  test 'BioPieces::Pipeline::WriteFasta with invalid options raises' do
+    assert_raise(@e) { @p.write_fasta(foo: 'bar') }
   end
 
-  test "BioPieces::Pipeline::WriteFasta to stdout outputs correctly" do
+  test 'BioPieces::Pipeline::WriteFasta to stdout outputs correctly' do
     result = capture_stdout { @p.write_fasta.run(input: @input) }
-    expected = ">test1\natcg\n>test2\ngtac\n"
-    assert_equal(expected, result)
+    assert_equal(@expected, result)
   end
 
-  test "BioPieces::Pipeline::WriteFasta status outputs correctly" do
+  test 'BioPieces::Pipeline::WriteFasta status outputs correctly' do
     capture_stdout { @p.write_fasta.run(input: @input) }
-    assert_equal(2, @p.status[:status].first[:sequences_out])
-    assert_equal(8, @p.status[:status].first[:residues_out])
+    assert_equal(2, @p.status.first[:sequences_out])
+    assert_equal(8, @p.status.first[:residues_out])
   end
 
-  test "BioPieces::Pipeline::WriteFasta with options[:wrap] outputs correctly" do
+  test 'BioPieces::Pipeline::WriteFasta with :wrap outputs correctly' do
     result = capture_stdout { @p.write_fasta(wrap: 2).run(input: @input) }
-    expected = ">test1\nat\ncg\n>test2\ngt\nac\n"
+
+    expected = <<-EXP.gsub(/^\s+\|/, '')
+      |>test1
+      |at
+      |cg
+      |>test2
+      |gt
+      |ac
+    EXP
+
     assert_equal(expected, result)
   end
 
-  test "BioPieces::Pipeline::WriteFasta to file outputs correctly" do
+  test 'BioPieces::Pipeline::WriteFasta to file outputs correctly' do
     @p.write_fasta(output: @file).run(input: @input, output: @output2)
-    result = File.read(@file)
-    expected = ">test1\natcg\n>test2\ngtac\n"
-    assert_equal(expected, result)
+
+    assert_equal(@expected, File.read(@file))
   end
 
-  test "BioPieces::Pipeline::WriteFasta to existing file raises" do
+  test 'BioPieces::Pipeline::WriteFasta to existing file raises' do
     `touch #{@file}`
-    assert_raise(BioPieces::OptionError) { @p.write_fasta(output: @file) }
+    assert_raise(@e) { @p.write_fasta(output: @file) }
   end
 
-  test "BioPieces::Pipeline::WriteFasta to existing file with options[:force] outputs correctly" do
+  test 'BioPieces::Pipeline::WriteFasta to file with :force outputs OK' do
     `touch #{@file}`
     @p.write_fasta(output: @file, force: true).run(input: @input)
-    result = File.open(@file).read
-    expected = ">test1\natcg\n>test2\ngtac\n"
-    assert_equal(expected, result)
+
+    assert_equal(@expected, File.open(@file).read)
   end
 
-  test "BioPieces::Pipeline::WriteFasta with gzipped data and no output file raises" do
-    assert_raise(BioPieces::OptionError) { @p.write_fasta(gzip: true) }
+  test 'BioPieces::Pipeline::WriteFasta with gzipdata and w/o file raises' do
+    assert_raise(@e) { @p.write_fasta(gzip: true) }
   end
 
-  test "BioPieces::Pipeline::WriteFasta with bzip2'ed data and no output file raises" do
-    assert_raise(BioPieces::OptionError) { @p.write_fasta(bzip2: true) }
+  test 'BioPieces::Pipeline::WriteFasta with bzip2 data w/o file raises' do
+    assert_raise(@e) { @p.write_fasta(bzip2: true) }
   end
 
-  test "BioPieces::Pipeline::WriteFasta to file outputs gzipped data correctly" do
+  test 'BioPieces::Pipeline::WriteFasta to file outputs gzipped data OK' do
     @p.write_fasta(output: @file, gzip: true).run(input: @input)
-    result = `#{@zcat} #{@file}`
-    expected = ">test1\natcg\n>test2\ngtac\n"
-    assert_equal(expected, result)
+
+    assert_equal(@expected, `#{@zcat} #{@file}`)
   end
 
-  test "BioPieces::Pipeline::WriteFasta to file outputs bzip2'ed data correctly" do
+  test 'BioPieces::Pipeline::WriteFasta to file outputs bzip2\'ed data OK' do
     @p.write_fasta(output: @file, bzip2: true).run(input: @input)
-    result = `bzcat #{@file}`
-    expected = ">test1\natcg\n>test2\ngtac\n"
-    assert_equal(expected, result)
+
+    assert_equal(@expected, `bzcat #{@file}`)
   end
 
-  test "BioPieces::Pipeline::WriteFasta with both gzip and bzip2 output raises" do
-    assert_raise(BioPieces::OptionError) { @p.write_fasta(output: @file, gzip: true, bzip2: true) }
+  test 'BioPieces::Pipeline::WriteFasta with gzip and bzip2 output raises' do
+    assert_raise(@e) { @p.write_fasta(output: @file, gzip: true, bzip2: true) }
   end
 
-  test "BioPieces::Pipeline::WriteFasta with flux outputs correctly" do
+  test 'BioPieces::Pipeline::WriteFasta with flux outputs correctly' do
     @p.write_fasta(output: @file).run(input: @input, output: @output2)
-    result = File.open(@file).read
-    expected = ">test1\natcg\n>test2\ngtac\n"
-    assert_equal(expected, result)
 
-    stream_result = @input2.map { |h| h.to_s }.reduce(:<<)
-    stream_expected = ""
-    stream_expected << '{:SEQ_NAME=>"test1", :SEQ=>"atcg", :SEQ_LEN=>4}'
-    stream_expected << '{:SEQ_NAME=>"test2", :SEQ=>"gtac", :SEQ_LEN=>4}'
-    assert_equal(stream_expected, stream_result)
+    expected2 = <<-EXP.gsub(/^\s+\|/, '')
+      |{:SEQ_NAME=>"test1", :SEQ=>"atcg", :SEQ_LEN=>4}
+      |{:SEQ_NAME=>"test2", :SEQ=>"gtac", :SEQ_LEN=>4}
+    EXP
+
+    assert_equal(@expected, File.open(@file).read)
+    assert_equal(expected2, collect_result)
   end
 end
