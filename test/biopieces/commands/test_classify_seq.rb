15101aa3459db72480971bd964e504f840693400
Martin A. Hansen <mail@maasha.dk>
2015-04-27 10:49:27 +0200

    classify_seq rewrite complete

---
+++ test/biopieces/commands/test_classify_seq.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                #
+#                                                                              #
+# This program is free software; you can redistribute it and/or                #
+# modify it under the terms of the GNU General Public License                  #
+# as published by the Free Software Foundation; either version 2               #
+# of the License, or (at your option) any later version.                       #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,    #
+# USA.                                                                         #
+#                                                                              #
+# http://www.gnu.org/copyleft/gpl.html                                         #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# This software is part of Biopieces (www.biopieces.org).                      #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+
+require 'test/helper'
+
+# Test class for ClassifySeq.
+class TestClassifySeq < Test::Unit::TestCase
+  def setup
+    @p = BP.new
+  end
+
+  test 'BioPieces::Pipeline#classify_seq with disallowed option raises' do
+    assert_raise(BioPieces::OptionError) do
+      @p.classify_seq(dir: Dir.pwd, foo: 'bar')
+    end
+  end
+
+  test 'BioPieces::Pipeline#classify_seq with allowed option dont raise' do
+    assert_nothing_raised { @p.classify_seq(dir: Dir.pwd) }
+  end
+
+  # test 'BioPieces::Pipeline#classify_seq outputs correctly' do
+  #   TODO: mock this sucker.
+  # end
+end
