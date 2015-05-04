b53affdb99a5506721fb1f0ae00239c4fe2b595a
Martin A. Hansen <mail@maasha.dk>
2015-05-04 16:59:30 +0200

    index_taxonomy rewrite complete

--- test/biopieces/commands/test_index_taxonomy.rb
+++ test/biopieces/commands/test_index_taxonomy.rb
@@ -1,37 +1,39 @@
 #!/usr/bin/env ruby
-$:.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                  #
-#                                                                                #
-# This program is free software; you can redistribute it and/or                  #
-# modify it under the terms of the GNU General Public License                    #
-# as published by the Free Software Foundation; either version 2                 #
-# of the License, or (at your option) any later version.                         #
-#                                                                                #
-# This program is distributed in the hope that it will be useful,                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-# GNU General Public License for more details.                                   #
-#                                                                                #
-# You should have received a copy of the GNU General Public License              #
-# along with this program; if not, write to the Free Software                    #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. #
-#                                                                                #
-# http://www.gnu.org/copyleft/gpl.html                                           #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# This software is part of Biopieces (www.biopieces.org).                        #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                #
+#                                                                              #
+# This program is free software; you can redistribute it and/or                #
+# modify it under the terms of the GNU General Public License                  #
+# as published by the Free Software Foundation; either version 2               #
+# of the License, or (at your option) any later version.                       #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,    #
+# USA.                                                                         #
+#                                                                              #
+# http://www.gnu.org/copyleft/gpl.html                                         #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# This software is part of Biopieces (www.biopieces.org).                      #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
 
 require 'test/helper'
 
-class TestIndexTaxonomy < Test::Unit::TestCase 
+# Test class for IndexTaxonomy.
+class TestIndexTaxonomy < Test::Unit::TestCase
   def setup
-    @tmpdir = Dir.mktmpdir("BioPieces")
+    @tmpdir = Dir.mktmpdir('BioPieces')
 
     @input, @output   = BioPieces::Stream.pipe
     @input2, @output2 = BioPieces::Stream.pipe
@@ -43,22 +45,16 @@ class TestIndexTaxonomy < Test::Unit::TestCase
     FileUtils.rm_r @tmpdir
   end
 
-  test "BioPieces::Pipeline::IndexTaxonomy with invalid options raises" do
-    assert_raise(BioPieces::OptionError) { @p.index_taxonomy(output_dir: @tmpdir, foo: "bar") }
+  test 'BioPieces::Pipeline::IndexTaxonomy with invalid options raises' do
+    assert_raise(BioPieces::OptionError) do
+      @p.index_taxonomy(output_dir: @tmpdir, foo: 'bar')
+    end
   end
 
-  test "BioPieces::Pipeline::IndexTaxonomy with valid options don't raise" do
-    assert_nothing_raised { @p.index_taxonomy(prefix: "foo", output_dir: @tmpdir) }
+  test 'BioPieces::Pipeline::IndexTaxonomy with valid options don\'t raise' do
+    assert_nothing_raised do
+      @p.index_taxonomy(output_dir: @tmpdir, kmer_size: 8, step_size: 1,
+                        prefix: 'foo')
+    end
   end
-
-#  test "BioPieces::Pipeline::IndexTaxonomy returns correctly" do
-#    @output.write({SEQ: "AT--C.G~"})
-#    @output.close
-#    @p.index_taxonomy(output_dir: @tmpdir).run(input: @input, output: @output2)
-#
-#    result   = @input2.map { |h| h.to_s }.reduce(:<<)
-#    expected = '{:SEQ=>"ATCG", :SEQ_LEN=>4}'
-#
-#    assert_equal(expected, result)
-#  end
 end
