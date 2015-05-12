381b81abc07c93b9c6434b494ef5c6a10095e08c
Martin A. Hansen <mail@maasha.dk>
2015-05-12 20:57:58 +0200

    reverse_seq rewrite complete

--- test/biopieces/commands/test_reverse_seq.rb
+++ test/biopieces/commands/test_reverse_seq.rb
@@ -1,44 +1,46 @@
 #!/usr/bin/env ruby
-$:.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                  #
-#                                                                                #
-# This program is free software; you can redistribute it and/or                  #
-# modify it under the terms of the GNU General Public License                    #
-# as published by the Free Software Foundation; either version 2                 #
-# of the License, or (at your option) any later version.                         #
-#                                                                                #
-# This program is distributed in the hope that it will be useful,                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-# GNU General Public License for more details.                                   #
-#                                                                                #
-# You should have received a copy of the GNU General Public License              #
-# along with this program; if not, write to the Free Software                    #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. #
-#                                                                                #
-# http://www.gnu.org/copyleft/gpl.html                                           #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# This software is part of Biopieces (www.biopieces.org).                        #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                #
+#                                                                              #
+# This program is free software; you can redistribute it and/or                #
+# modify it under the terms of the GNU General Public License                  #
+# as published by the Free Software Foundation; either version 2               #
+# of the License, or (at your option) any later version.                       #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,    #
+# USA.                                                                         #
+#                                                                              #
+# http://www.gnu.org/copyleft/gpl.html                                         #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# This software is part of Biopieces (www.biopieces.org).                      #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
 
 require 'test/helper'
 
-class TestReverseSeq < Test::Unit::TestCase 
+# Test class for ReverseSeq.
+class TestReverseSeq < Test::Unit::TestCase
   def setup
     @input, @output   = BioPieces::Stream.pipe
     @input2, @output2 = BioPieces::Stream.pipe
 
     hash = {
-      SEQ_NAME: "test",
-      SEQ: "gatcgatcgt",
+      SEQ_NAME: 'test',
+      SEQ: 'gatcgatcgt',
       SEQ_LEN: 10,
-      SCORES: "ABCDEFGHII"
+      SCORES: 'ABCDEFGHII'
     }
 
     @output.write hash
@@ -47,17 +49,20 @@ class TestReverseSeq < Test::Unit::TestCase
     @p = BioPieces::Pipeline.new
   end
 
-  test "BioPieces::Pipeline::ReverseSeq with invalid options raises" do
-    assert_raise(BioPieces::OptionError) { @p.reverse_seq(foo: "bar") }
+  test 'BioPieces::Pipeline::ReverseSeq with invalid options raises' do
+    assert_raise(BioPieces::OptionError) { @p.reverse_seq(foo: 'bar') }
   end
 
-  test "BioPieces::Pipeline::ReverseSeq returns correctly" do
+  test 'BioPieces::Pipeline::ReverseSeq returns correctly' do
     @p.reverse_seq.run(input: @input, output: @output2)
 
-    result   = @input2.map { |h| h.to_s }.reduce(:<<)
-    expected = ""
-    expected << %Q{{:SEQ_NAME=>"test", :SEQ=>"tgctagctag", :SEQ_LEN=>10, :SCORES=>"IIHGFEDCBA"}}
+    expected = <<-EXP.gsub(/^\s+\|/, '')
+      |{:SEQ_NAME=>"test",
+      | :SEQ=>"tgctagctag",
+      | :SEQ_LEN=>10,
+      | :SCORES=>"IIHGFEDCBA"}
+    EXP
 
-    assert_equal(expected, result)
+    assert_equal(expected.delete("\n"), collect_result.delete("\n"))
   end
 end
