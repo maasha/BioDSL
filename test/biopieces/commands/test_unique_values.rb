8ecb4b9de0a9f9ac30bbc575cc781ab5c104d344
Martin A. Hansen <mail@maasha.dk>
2015-05-20 14:05:32 +0200

    unique_values rewrite complete

--- test/biopieces/commands/test_unique_values.rb
+++ test/biopieces/commands/test_unique_values.rb
@@ -1,46 +1,48 @@
 #!/usr/bin/env ruby
-$:.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                  #
-#                                                                                #
-# This program is free software; you can redistribute it and/or                  #
-# modify it under the terms of the GNU General Public License                    #
-# as published by the Free Software Foundation; either version 2                 #
-# of the License, or (at your option) any later version.                         #
-#                                                                                #
-# This program is distributed in the hope that it will be useful,                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-# GNU General Public License for more details.                                   #
-#                                                                                #
-# You should have received a copy of the GNU General Public License              #
-# along with this program; if not, write to the Free Software                    #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. #
-#                                                                                #
-# http://www.gnu.org/copyleft/gpl.html                                           #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# This software is part of Biopieces (www.biopieces.org).                        #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                #
+#                                                                              #
+# This program is free software; you can redistribute it and/or                #
+# modify it under the terms of the GNU General Public License                  #
+# as published by the Free Software Foundation; either version 2               #
+# of the License, or (at your option) any later version.                       #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,    #
+# USA.                                                                         #
+#                                                                              #
+# http://www.gnu.org/copyleft/gpl.html                                         #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# This software is part of Biopieces (www.biopieces.org).                      #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
 
 require 'test/helper'
 
-class TestUniqueValues < Test::Unit::TestCase 
+# Test class for UniqueValues.
+class TestUniqueValues < Test::Unit::TestCase
   def setup
     @input, @output   = BioPieces::Stream.pipe
     @input2, @output2 = BioPieces::Stream.pipe
 
-    [ {V0: "HUMAN", V1: "H1"},
-      {V0: "HUMAN", V1: "H2"},
-      {V0: "HUMAN", V1: "H3"},
-      {V0: "DOG",   V1: "D1"},
-      {V0: "DOG",   V1: "D2"},
-      {V0: "MOUSE", V1: "M1"},
-      {FOO: "BAR"}
+    [{V0: 'HUMAN', V1: 'H1'},
+     {V0: 'HUMAN', V1: 'H2'},
+     {V0: 'HUMAN', V1: 'H3'},
+     {V0: 'DOG',   V1: 'D1'},
+     {V0: 'DOG',   V1: 'D2'},
+     {V0: 'MOUSE', V1: 'M1'},
+     {FOO: 'BAR'}
     ].each do |record|
       @output.write record
     end
@@ -50,37 +52,40 @@ class TestUniqueValues < Test::Unit::TestCase
     @p = BioPieces::Pipeline.new
   end
 
-  test "BioPieces::Pipeline#unique_values with disallowed option raises" do
-    assert_raise(BioPieces::OptionError) { @p.unique_values(key: :V0, foo: "bar") }
+  test 'BioPieces::Pipeline#unique_values with disallowed option raises' do
+    assert_raise(BioPieces::OptionError) do
+      @p.unique_values(key: :V0, foo: 'bar')
+    end
   end
 
-  test "BioPieces::Pipeline#unique_values with allowed options don't raise" do
+  test 'BioPieces::Pipeline#unique_values with allowed options dont raise' do
     assert_nothing_raised { @p.unique_values(key: :V0) }
   end
 
-  test "BioPieces::Pipeline#unique_values returns correctly" do
-    @p.unique_values(key: "V0").run(input: @input, output: @output2)
+  test 'BioPieces::Pipeline#unique_values returns correctly' do
+    @p.unique_values(key: 'V0').run(input: @input, output: @output2)
 
-    result   = @input2.map { |h| h.to_s }.reduce(:<<)
-    expected = ""
-    expected << '{:V0=>"HUMAN", :V1=>"H1"}'
-    expected << '{:V0=>"DOG", :V1=>"D1"}'
-    expected << '{:V0=>"MOUSE", :V1=>"M1"}'
-    expected << '{:FOO=>"BAR"}'
+    expected = <<-EXP.gsub(/^\s+\|/, '')
+      |{:V0=>"HUMAN", :V1=>"H1"}
+      |{:V0=>"DOG", :V1=>"D1"}
+      |{:V0=>"MOUSE", :V1=>"M1"}
+      |{:FOO=>"BAR"}
+    EXP
 
-    assert_equal(expected, result)
+    assert_equal(expected, collect_result)
   end
 
-  test "BioPieces::Pipeline#unique_values with options[:invert] returns correctly" do
-    @p.unique_values(key: "V0", invert: true).run(input: @input, output: @output2)
+  test 'BioPieces::Pipeline#unique_values with :invert returns correctly' do
+    @p.unique_values(key: 'V0', invert: true).
+      run(input: @input, output: @output2)
 
-    result   = @input2.map { |h| h.to_s }.reduce(:<<)
-    expected = ""
-    expected << '{:V0=>"HUMAN", :V1=>"H2"}'
-    expected << '{:V0=>"HUMAN", :V1=>"H3"}'
-    expected << '{:V0=>"DOG", :V1=>"D2"}'
-    expected << '{:FOO=>"BAR"}'
+    expected = <<-EXP.gsub(/^\s+\|/, '')
+      |{:V0=>"HUMAN", :V1=>"H2"}
+      |{:V0=>"HUMAN", :V1=>"H3"}
+      |{:V0=>"DOG", :V1=>"D2"}
+      |{:FOO=>"BAR"}
+    EXP
 
-    assert_equal(expected, result)
+    assert_equal(expected, collect_result)
   end
 end
