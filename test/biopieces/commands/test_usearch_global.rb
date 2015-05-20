63bc33abcc3c79bccb323da2b09eafa495c09dc7
Martin A. Hansen <mail@maasha.dk>
2015-05-20 16:08:45 +0200

    usearch_global rewrite complete

--- test/biopieces/commands/test_usearch_global.rb
+++ test/biopieces/commands/test_usearch_global.rb
@@ -1,70 +1,104 @@
 #!/usr/bin/env ruby
-$:.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                  #
-#                                                                                #
-# This program is free software; you can redistribute it and/or                  #
-# modify it under the terms of the GNU General Public License                    #
-# as published by the Free Software Foundation; either version 2                 #
-# of the License, or (at your option) any later version.                         #
-#                                                                                #
-# This program is distributed in the hope that it will be useful,                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-# GNU General Public License for more details.                                   #
-#                                                                                #
-# You should have received a copy of the GNU General Public License              #
-# along with this program; if not, write to the Free Software                    #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. #
-#                                                                                #
-# http://www.gnu.org/copyleft/gpl.html                                           #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
-#                                                                                #
-# This software is part of Biopieces (www.biopieces.org).                        #
-#                                                                                #
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# Copyright (C) 2007-2015 Martin Asser Hansen (mail@maasha.dk).                #
+#                                                                              #
+# This program is free software; you can redistribute it and/or                #
+# modify it under the terms of the GNU General Public License                  #
+# as published by the Free Software Foundation; either version 2               #
+# of the License, or (at your option) any later version.                       #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,    #
+# USA.                                                                         #
+#                                                                              #
+# http://www.gnu.org/copyleft/gpl.html                                         #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# This software is part of Biopieces (www.biopieces.org).                      #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
 
 require 'test/helper'
 
-class TestUsearchGlobal < Test::Unit::TestCase 
+# Test class for UsearchGlobal.
+class TestUsearchGlobal < Test::Unit::TestCase
+  require 'tempfile'
+
   def setup
-    omit("usearch not found") unless BioPieces::Filesys.which("usearch")
+    omit('usearch not found') unless BioPieces::Filesys.which('usearch')
 
-    @db = File.join(File.dirname(__FILE__), '..', '..', '..', 'data', 'chimera_db.fna')
+    data = <<-DAT.gsub(/^\s+\|/, '')
+      |>test1
+      |gtgtgtagctacgatcagctagcgatcgagctatatgttt
+    DAT
+
+    @db = Tempfile.new('database')
+
+    File.open(@db, 'w') do |ios|
+      ios << data
+    end
   end
 
-  test "BioPieces::Pipeline#usearch_global with disallowed option raises" do
+  def teardown
+    @db.close
+    @db.unlink
+  end
+
+  test 'BioPieces::Pipeline#usearch_global with disallowed option raises' do
     p = BioPieces::Pipeline.new
-    assert_raise(BioPieces::OptionError) { p.usearch_global(foo: "bar") }
+    assert_raise(BioPieces::OptionError) { p.usearch_global(foo: 'bar') }
   end
 
-  test "BioPieces::Pipeline#usearch_global with allowed option don't raise" do
+  test 'BioPieces::Pipeline#usearch_global with allowed option dont raise' do
     p = BioPieces::Pipeline.new
-    assert_nothing_raised { p.usearch_global(database: @db, identity: 1) }
+    assert_nothing_raised { p.usearch_global(database: @db.path, identity: 1) }
   end
 
-  test "BioPieces::Pipeline#usearch_global outputs correctly" do
-    input, output   = BioPieces::Stream.pipe
-    input2, output2 = BioPieces::Stream.pipe
+  test 'BioPieces::Pipeline#usearch_global outputs correctly' do
+    input, output    = BioPieces::Stream.pipe
+    @input2, output2 = BioPieces::Stream.pipe
 
-    output.write({one: 1, two: 2, three: 3})
-    output.write({SEQ: "gtgtgtagctacgatcagctagcgatcgagctatatgttt"})
-    output.write({SEQ: "atcgatcgatcgatcgatcgatcgatcgtacgacgtagct"})
+    output.write(one: 1, two: 2, three: 3)
+    output.write(SEQ: 'gtgtgtagctacgatcagctagcgatcgagctatatgttt')
+    output.write(SEQ: 'atcgatcgatcgatcgatcgatcgatcgtacgacgtagct')
     output.close
 
     p = BioPieces::Pipeline.new
-    p.usearch_global(database: @db, identity: 0.97, strand: "plus").run(input: input, output: output2)
-    result   = input2.map { |h| h.to_s }.sort_by { |a| a.to_s }.reduce(:<<)
-    expected = ""
-    expected << %Q{{:SEQ=>"atcgatcgatcgatcgatcgatcgatcgtacgacgtagct"}}
-    expected << %Q{{:SEQ=>"gtgtgtagctacgatcagctagcgatcgagctatatgttt"}}
-    expected << %Q{{:TYPE=>"H", :CLUSTER=>0, :SEQ_LEN=>40, :IDENT=>100.0, :STRAND=>"+", :CIGAR=>"40M", :Q_ID=>"1", :S_ID=>"test1", :RECORD_TYPE=>"usearch"}}
-    expected << %Q{{:TYPE=>"N", :CLUSTER=>0, :SEQ_LEN=>0, :STRAND=>".", :CIGAR=>"*", :Q_ID=>"2", :RECORD_TYPE=>"usearch"}}
-    expected << %Q{{:one=>1, :two=>2, :three=>3}}
+    p.usearch_global(database: @db.path, identity: 0.97, strand: 'plus').
+      run(input: input, output: output2)
 
-    assert_equal(expected, result)
+    expected = <<-EXP.gsub(/^\s+\|/, '')
+      |{:SEQ=>"atcgatcgatcgatcgatcgatcgatcgtacgacgtagct"}
+      |{:SEQ=>"gtgtgtagctacgatcagctagcgatcgagctatatgttt"}
+      |{:TYPE=>"H",
+      | :CLUSTER=>0,
+      | :SEQ_LEN=>40,
+      | :IDENT=>100.0,
+      | :STRAND=>"+",
+      | :CIGAR=>"40M",
+      | :Q_ID=>"1",
+      | :S_ID=>"test1",
+      | :RECORD_TYPE=>"usearch"}
+      |{:TYPE=>"N",
+      | :CLUSTER=>0,
+      | :SEQ_LEN=>0,
+      | :STRAND=>".",
+      | :CIGAR=>"*",
+      | :Q_ID=>"2",
+      | :RECORD_TYPE=>"usearch"}
+      |{:one=>1, :two=>2, :three=>3}
+    EXP
+
+    assert_equal(expected.delete("\n"), collect_sorted_result.delete("\n"))
   end
 end
